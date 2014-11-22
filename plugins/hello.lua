80af530754e12b934097c9d94cf6131b3ae61c80
yago <yagoperezs@gmail.com>
2014-11-22 14:42:26 +0100

    hello space

--- plugins/hello.lua
+++ plugins/hello.lua
@@ -1,12 +1,15 @@
 
 function run(msg, matches)
-  return "Hello," .. matches[1]
+  return "Hello, " .. matches[1]
 end
 
 return {
     description = "Says hello to someone", 
     usage = "say hello to [name]",
-    patterns = {"^say hello to (.*)$"}, 
+    patterns = {
+    	"^say hello to (.*)$".
+    	"^Say hello to (.*)$"
+    }, 
     run = run 
 }
 
