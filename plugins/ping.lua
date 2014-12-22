7881f62e2094cd960d70ec1d051a89b8302e00e7
yago <yagoperezs@gmail.com>
2014-12-22 21:43:36 +0100

    closed tcp conexion

--- plugins/ping.lua
+++ plugins/ping.lua
@@ -3,12 +3,15 @@ socket = require("socket")
 function cron()
 	-- Checks a TCP connexion
 	-- Use yours desired web and id
-	local addr = "your.web.site"
-	local dest = "user#id"..our_id 
-    if not socket.connect(addr, 80) then 
+	local addr = "gul.es"
+	local dest = "user#id"..our_id
+    local connexion = socket.connect(addr, 80)
+    if not connexion then 
     	local text = "ALERT: "..addr.." is offline"
     	print (text)
     	send_msg(dest, text, ok_cb, false)
+    else
+        connexion:close()
     end
 end
 
