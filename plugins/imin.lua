4d294f1dbc2df76545ac204dd8f932ea214181d9
SinaKK <SinaKK@users.noreply.github.com>
2015-01-30 15:57:40 +0800

    Update imin.lua

--- plugins/imin.lua
+++ plugins/imin.lua
@@ -37,7 +37,7 @@ function join_event(chat, text,user)
 	  return "Event doesn't exists..."
 	end
 	
-	table.insert(_values[chat][eventname], user)
+	_values[chat][eventname][user] = true
 
 	-- Save values to file
 	serialize_to_file(_values, _file_values)
