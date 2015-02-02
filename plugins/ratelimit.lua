9a7f74d19d31bd3bd04a8e8b1709bb87e3536b4b
SinaKK <SinaKK@users.noreply.github.com>
2015-02-02 14:02:12 +0800

    Update ratelimit.lua

--- plugins/ratelimit.lua
+++ plugins/ratelimit.lua
@@ -3,7 +3,7 @@
 function run(msg, matches)
    limit = string.match(msg.text, "!ratelimit (%d+)")
    ratelimit = limit
-   return "Ratelimit set: "..limit.."s"
+   return "Image Ratelimit set: "..limit.."s"
 end
 
 return {
