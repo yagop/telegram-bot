1b31670c99d1fdf95082148fd9628a9c1f00e7b4
SinaKK <SinaKK@users.noreply.github.com>
2015-02-02 13:42:51 +0800

    Create ratelimit.lua

---
+++ plugins/ratelimit.lua
@@ -0,0 +1,20 @@
+function delay_s(delay)
+   delay = delay or 1
+   local time_to = os.time() + delay
+   while os.time() < time_to do end
+end
+
+function run(msg, matches)
+   seconds = string.match(msg.text, "!shutup (%d+)")
+   shutup = os.time() + seconds
+   return "Zzz 😴"
+end
+
+return {
+    description = "shut the bot up", 
+    usage = "!shutup seconds",
+    patterns = {"^!shutup (%d+)$"}, 
+    run = run 
+}
+
+
