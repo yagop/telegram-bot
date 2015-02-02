2e2f5073d67609f19d4e52717aa8343500706fc2
SinaKK <SinaKK@users.noreply.github.com>
2015-02-02 13:29:10 +0800

    Update shutup.lua

--- plugins/shutup.lua
+++ plugins/shutup.lua
@@ -6,27 +6,15 @@ function delay_s(delay)
 end
 
 function run(msg, matches)
-  local lat = matches[1]
-  local lon = matches[2]
-  local receiver = get_receiver(msg)
-
-  local zooms = {16, 18}
-
-  for i = 1, #zooms do
-    local zoom = zooms[i]
-    local url = "http://maps.googleapis.com/maps/api/staticmap?zoom=" .. zoom .. "&size=600x300&maptype=roadmap&center=" .. lat .. "," .. lon .. "&markers=color:blue%7Clabel:X%7C" .. lat .. "," .. lon
-    local file = download_to_file(url)
-    send_photo(receiver, file, ok_cb, false)
-    delay_s(2)
-  end
-
-  return "www.google.es/maps/place/@" .. lat .. "," .. lon
+   seconds = string.match(msg.text, "!shutup (%d+)")
+   send_msg(get_reciever(receiver), "Zzz 😴", ok_cb, false)
+   delay_s(seconds)
 end
 
 return {
-    description = "generates a map showing the given GPS coordinates", 
-    usage = "!gps latitude,longitude",
-    patterns = {"^!gps ([^,]*)[,%s]([^,]*)$"}, 
+    description = "shut the bot up", 
+    usage = "!shutup seconds",
+    patterns = {"^!shutup (%d+)$"}, 
     run = run 
 }
 
