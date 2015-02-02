b0803cb4c9e49ee643bfba1e5d6ff0b96fdc8765
SinaKK <SinaKK@users.noreply.github.com>
2015-02-02 10:46:41 +0800

    Update wiki.lua

--- plugins/wiki.lua
+++ plugins/wiki.lua
@@ -1,38 +1,67 @@
-function googlethat(query)
-  local api        = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&"
-  local parameters = "q=".. (URL.escape(query) or "")
+require("./plugins/google")
 
-  -- Do the request
-  local res, code = https.request(api..parameters)
-  if code ~=200 then return nil  end
-  local data = json:decode(res)
-  local results={}
-  for key,result in ipairs(data.responseData.results) do
-    table.insert(results,{result.titleNoFormatting, result.url})
-  end
-  return results
+function firstresult(results)
+  return results[1][2]
 end
 
-function stringlinks(results)
-  local stringresults=""
-  for key,val in ipairs(results) do
-    stringresults=stringresults..val[1].." - "..val[2].."\n"
+function wikiapilink(wikiurl)
+  local wikipattern="^http://en.wikipedia.org/wiki/(.*)$"
+  local matches = { string.match(wikiurl, wikipattern) }
+  local queryterm=matches[1]
+  local apilink = "http://en.wikipedia.org/w/index.php?action=raw&title="..queryterm
+  return apilink
+end
+
+
+function getwikifromapi(apilink)
+  -- Do the request
+  local res, code = https.request(apilink)
+  if code ~=200 then return "An error occured" end
+  local data = res
+  local wikitext=data
+  return wikitext
+end
+
+function plaintext(wikitext)
+  local plaintext=string.gsub(wikitext,"{{Infobox.-\n}}\n","")
+  plaintext=string.gsub(plaintext,"{{.-}}","")
+  plaintext=string.gsub(plaintext,"[%[%|%]%]]","")
+  plaintext=string.gsub(plaintext,"<ref.->.-</ref>","")
+  local firstsectionindex=string.find(plaintext,"==")
+  if firstsectionindex~=nil then
+    plaintext=string.sub(plaintext,1,firstsectionindex-1)
   end
-  return stringresults
+  return plaintext
+end
+
+function formatwikiforsending(wikitext,url)
+  -- local stringtosend=title.."\n==="
+  local stringtosend=plaintext(wikitext).."\n"
+  stringtosend=stringtosend.."- "..url
+  return stringtosend
+end
+
+
+function scrapewiki(results)
+  local url=firstresult(results)
+  local tosend=formatwikiforsending(getwikifromapi(wikiapilink(url)),url)
+  return tosend
 end
 
 function run(msg, matches)
-   vardump(matches)
-   local results = googlethat(matches[1])
-   return stringlinks(results)
+  vardump(matches)
+  local results = googlethat(matches[1].." site:en.wikipedia.org")
+  return scrapewiki(results)
 end
 
+
 return {
-   description = "Searches Google and send results",
-   usage = "!google [terms]",
-   patterns = {
-      "^!google (.*)$",
-      "^%.[g|G]oogle (.*)$"
-   },
-   run = run
-}
+  description = "Gives you the wikipedia article you are looking for",
+  usage = "!wiki terms",
+  patterns = {
+    "^!wiki (.*)$",
+    "^%.[w|W]iki (.*)$"
+    },
+    run = run
+  }
+
