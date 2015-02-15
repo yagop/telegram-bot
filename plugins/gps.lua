e0dd062cfe135230e2857330cc6f9f143fa8f2dd
yago <yagoperezs@gmail.com>
2015-02-15 16:59:03 +0100

    gps.lua no delay_s

--- plugins/gps.lua
+++ plugins/gps.lua
@@ -1,24 +1,17 @@
-
-function delay_s(delay)
-   delay = delay or 1
-   local time_to = os.time() + delay
-   while os.time() < time_to do end
-end
-
 function run(msg, matches)
   local lat = matches[1]
   local lon = matches[2]
   local receiver = get_receiver(msg)
 
   local zooms = {16, 18}
-
+  local urls = {}
   for i = 1, #zooms do
     local zoom = zooms[i]
     local url = "http://maps.googleapis.com/maps/api/staticmap?zoom=" .. zoom .. "&size=600x300&maptype=roadmap&center=" .. lat .. "," .. lon .. "&markers=color:blue%7Clabel:X%7C" .. lat .. "," .. lon
-    local file = download_to_file(url)
-    send_photo(receiver, file, ok_cb, false)
-    delay_s(2)
+    table.insert(urls, url)
   end
+
+  send_photos_from_url(receiver, urls)
 
   return "www.google.es/maps/place/@" .. lat .. "," .. lon
 end
