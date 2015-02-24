68ccf0bca6d140b83478f09d103e42a11dcb4d68
SinaKK <SinaKK@users.noreply.github.com>
2015-02-24 12:30:26 +0800

    Update whosfree.lua

--- plugins/whosfree.lua
+++ plugins/whosfree.lua
@@ -1,14 +1,34 @@
 
 function run(msg, matches)
+   day = string.match(text, "[!|.]deets (.+)")
+   text = ""
    -- Do the request
-   print("test")
-   local res, code = http.request("http://www.rockym93.net/code/titp2/titp_now.py")
-   print(res)
-   print(code)
-  if code == 200 then 
-   return "Free now:\n"..res.."\n\n via TITP (rockym93.net)"
-  end
+   if day == nil then
+      print("test")
+      local res, code = http.request("http://www.rockym93.net/code/titp2/titp_now.py")
+      print(res)
+      print(code)
+     if code == 200 then 
+      text = text + "Free now:\n"..res.."\n\n via TITP (rockym93.net)"
+     end
+     return text
+   end
+   if day == "today" or day == "Today" then
+     print("test")
+     local res, code = http.request("http://www.rockym93.net/code/titp2/timetable.json")
+     print(res)
+     print(code)
+     if code == 200 then 
+      local tt = JSON:decode(res) 
+      tt.remove(0)
+      text = "Today\n\n"
+      for day,users in pairs(tt) then
+         text = text .. day .. users.."\n"
+      end
+      return text
+   end
 end
+
 
 
 return {
@@ -16,6 +36,7 @@ return {
   usage = "!whosfree",
   patterns = {
     "^[!|.]whosfree$",
+    "^[!|.]whosfree (.+)"$
     },
     run = run
   }
