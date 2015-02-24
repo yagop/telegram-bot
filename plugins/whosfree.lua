c5fb9e6782c977b2076458bb4f77979bf3ddf7af
SinaKK <SinaKK@users.noreply.github.com>
2015-02-24 12:39:54 +0800

    Update whosfree.lua

--- plugins/whosfree.lua
+++ plugins/whosfree.lua
@@ -13,20 +13,6 @@ function run(msg, matches)
      end
      return text
    end
-   if day == "today" or day == "Today" then
-     print("test")
-     local res, code = http.request("http://www.rockym93.net/code/titp2/timetable.json")
-     print(res)
-     print(code)
-     if code == 200 then 
-      local tt = JSON:decode(res) 
-      tt.remove(0)
-      text = "Today\n\n"
-      for day,users in pairs(tt) then
-         text = text .. day .. users.."\n"
-      end
-      return text
-   end
 end
 
 
@@ -36,7 +22,6 @@ return {
   usage = "!whosfree",
   patterns = {
     "^[!|.]whosfree$",
-    "^[!|.]whosfree (.+)$"
     },
     run = run
   }
