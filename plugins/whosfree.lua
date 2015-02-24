0a2934c33dcfa971bf8ea3ec53224cc748241aca
SinaKK <SinaKK@users.noreply.github.com>
2015-02-24 10:52:59 +0800

    Update whosfree.lua

--- plugins/whosfree.lua
+++ plugins/whosfree.lua
@@ -1,7 +1,10 @@
 
 function run(msg, matches)
    -- Do the request
+   print("test")
    local res, code = https.request("http://www.rockym93.net/code/titp2/titp_now.py")
+   print(res)
+   print(code)
   if code == 200 then 
    return "Free now:\n"..res.."\n\n via TITP (rockym93.net)"
   end
