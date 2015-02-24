c6a88c9bd7203e5dfb1a024eb1ad466a158659d9
SinaKK <SinaKK@users.noreply.github.com>
2015-02-24 10:40:51 +0800

    Update whosfree.lua

--- plugins/whosfree.lua
+++ plugins/whosfree.lua
@@ -2,9 +2,6 @@
 function run(msg, matches)
    -- Do the request
    return "Working" 
-  local res, code = https.request("http://www.rockym93.net/code/titp2/titp_now.py")
-  if code ~=200 then return "An error occured" end
-  return "Free now:\n"..res.."\n\n via TITP (rockym93.net)"
 end
 
 
