83e24baf2daf237cde44c7579420d8bd49648c01
SinaKK <SinaKK@users.noreply.github.com>
2015-02-24 10:54:32 +0800

    Update whosfree.lua

--- plugins/whosfree.lua
+++ plugins/whosfree.lua
@@ -2,7 +2,7 @@
 function run(msg, matches)
    -- Do the request
    print("test")
-   local res, code = https.request("http://www.rockym93.net/code/titp2/titp_now.py")
+   local res, code = http.request("http://www.rockym93.net/code/titp2/titp_now.py")
    print(res)
    print(code)
   if code == 200 then 
