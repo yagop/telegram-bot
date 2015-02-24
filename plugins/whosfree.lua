646b26dff7bcd3470ed35db7b305a3056c9e89e3
SinaKK <SinaKK@users.noreply.github.com>
2015-02-24 14:59:16 +0800

    Update whosfree.lua

--- plugins/whosfree.lua
+++ plugins/whosfree.lua
@@ -1,4 +1,33 @@
 
+
+function format_time(timestamp, format, tzoffset, tzname)
+   if tzoffset == "local" then  -- calculate local time zone (for the server)
+      local now = os.time()
+      local local_t = os.date("*t", now)
+      local utc_t = os.date("!*t", now)
+      local delta = (local_t.hour - utc_t.hour)*60 + (local_t.min - utc_t.min)
+      local h, m = math.modf( delta / 60)
+      tzoffset = string.format("%+.4d", 100 * h + 60 * m)
+   end
+   tzoffset = tzoffset or "GMT"
+   format = format:gsub("%%z", tzname or tzoffset)
+   if tzoffset == "GMT" then
+      tzoffset = "+0000"
+   end
+   tzoffset = tzoffset:gsub(":", "")
+
+   local sign = 1
+   if tzoffset:sub(1,1) == "-" then
+      sign = -1
+      tzoffset = tzoffset:sub(2)
+   elseif tzoffset:sub(1,1) == "+" then
+      tzoffset = tzoffset:sub(2)
+   end
+   tzoffset = sign * (tonumber(tzoffset:sub(1,2))*60 +
+      tonumber(tzoffset:sub(3,4)))*60
+   return os.date(format, timestamp + tzoffset)
+end
+
 function run(msg, matches)
    day = string.match(msg.text, "[!|.]whosfree (.+)")
    text = ""
@@ -21,7 +50,7 @@ function run(msg, matches)
       if code == 200 then 
          JSON = assert(loadfile "libs/JSON.lua")()
          local tt = JSON:decode(res)
-         today = os.date("%A") 
+         today = format_time(os.time(), "%A", "+08:00")
          tt["users"] = nil
          print(tt)
          text = today.. "\n"
