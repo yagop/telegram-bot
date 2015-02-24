1dd414892615d3537af11a2fdbde5607fd34e3ed
SinaKK <SinaKK@users.noreply.github.com>
2015-02-24 14:17:28 +0800

    Update whosfree.lua

--- plugins/whosfree.lua
+++ plugins/whosfree.lua
@@ -22,7 +22,7 @@ function run(msg, matches)
          JSON = assert(loadfile "libs/JSON.lua")()
          local tt = JSON:decode(res)
          today = "Tuesday"
-         tt.remove(0)
+         table.remove(tt,1)
          print(tt)
          text = "Today\n\n"
          for day,hours in pairs(tt) do
