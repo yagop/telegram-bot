230fbc0177cabf02bdfef497c7fc02f89c535879
Yago <yagop@users.noreply.github.com>
2015-03-04 23:22:00 +0100

    Fix DOT extension.

--- plugins/images.lua
+++ plugins/images.lua
@@ -10,9 +10,9 @@ return {
   description = "When user sends image URL (ends with png, jpg, jpeg) download and send it to origin.", 
   usage = "",
   patterns = {
-    "(https?://[%w-_%.%?%.:/%+=&]+.png)$",
-    "(https?://[%w-_%.%?%.:/%+=&]+.jpg)$",
-    "(https?://[%w-_%.%?%.:/%+=&]+.jpeg)$",
+    "(https?://[%w-_%.%?%.:/%+=&]+%.png)$",
+    "(https?://[%w-_%.%?%.:/%+=&]+%.jpg)$",
+    "(https?://[%w-_%.%?%.:/%+=&]+%.jpeg)$",
   }, 
   run = run 
 }
