6c9cc5c247139bf91301a356cce74751fa485192
AckiyBolt <ackiybolt@gmail.com>
2015-03-29 19:57:20 +0300

    plg updates

--- plugins/echo.lua
+++ plugins/echo.lua
@@ -1,12 +1,22 @@
 
 function run(msg, matches)
-  return matches[1]
+
+	local count = tonumber(matches[1])
+	local result = ""
+
+	while (count > 0 ) do
+		result = result .. matches[2] .. "\n"
+		count = count - 1
+		print (result)
+	end
+
+  	return result
 end
 
 return {
     description = "Simplest plugin ever!",
-    usage = "!echo [whatever]: echoes the msg",
-    patterns = {"^!echo (.*)$"}, 
+    usage = "!echo [count] [whatever]: echoes the msg N times",
+    patterns = {"^!echo (%d) (.*)$"}, 
     run = run 
 }
 
