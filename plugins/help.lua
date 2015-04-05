0c2bb7329c27d0190f3a7c13b9f5ea8e7800e85f
yago <yagoperezs@gmail.com>
2015-04-05 16:20:19 +0200

    Removed help markdown function

--- plugins/help.lua
+++ plugins/help.lua
@@ -1,37 +1,5 @@
 do
 
--- Generate an HTML table for GitHub
-local function html_help()
-  local text = [[<table>
-    <thead>
-      <tr>
-        <td><strong>Name</strong></td>
-        <td><strong>Description</strong></td>
-        <td><strong>Usage</strong></td>
-      </tr>
-    </thead>
-    <tbody>]]
-
-  for k,v in pairs(plugins_names()) do
-    t = loadfile("plugins/"..v)()
-    text = text.."<tr>"
-    text = text.."<td>"..v.."</td>"
-    text = text.."<td>"..t.description.."</td>"
-    text = text.."<td>"
-    if (type(t.usage) == "table") then
-      for ku,vu in pairs(t.usage) do
-        text = text..vu.."<br>"
-      end
-    else
-      text = text..t.usage
-    end
-    text = text.."</td>"
-    text = text.."</tr>"
-  end
-  text = text.."</tbody></table>"
-  return text
-end
-
 -- Returns true if is not empty
 local function has_usage_data(dict)
   if (dict.usage == nil or dict.usage == '') then
