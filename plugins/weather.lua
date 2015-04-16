dc8aa698152b5b2de0bb595d2278f7098307931a
AckiyBolt <ackiybolt@gmail.com>
2015-04-16 22:22:01 +0300

    weather prints wind, default loc is Kyiv

--- plugins/weather.lua
+++ plugins/weather.lua
@@ -23,22 +23,42 @@ function get_weather(location)
     conditions = conditions .. ' ☔☔☔☔'
   end
 
-  return temp .. '\n' .. conditions
+  wind = "Wind are " .. weather.wind.speed .. "mps"
+
+
+  if     weather.wind.deg > 326.25 and weather.wind.deg <= 348.75 then wind = wind .. ' NNW'
+  elseif weather.wind.deg > 303.75 and weather.wind.deg <= 326.25 then wind = wind .. ' NW'
+  elseif weather.wind.deg > 281.25 and weather.wind.deg <= 303.75 then wind = wind .. ' WNW'
+  elseif weather.wind.deg > 258.75 and weather.wind.deg <= 281.25 then wind = wind .. ' W'
+  elseif weather.wind.deg > 236.25 and weather.wind.deg <= 258.75 then wind = wind .. ' WSW'
+  elseif weather.wind.deg > 213.75 and weather.wind.deg <= 236.25 then wind = wind .. ' SW'
+  elseif weather.wind.deg > 191.25 and weather.wind.deg <= 213.75 then wind = wind .. ' SSW'
+  elseif weather.wind.deg > 168.75 and weather.wind.deg <= 191.25 then wind = wind .. ' S'
+  elseif weather.wind.deg > 146.25 and weather.wind.deg <= 168.75 then wind = wind .. ' SSE'
+  elseif weather.wind.deg > 101.25 and weather.wind.deg <= 123.75 then wind = wind .. ' ESE'
+  elseif weather.wind.deg > 78.75  and weather.wind.deg <= 101.25 then wind = wind .. ' E'
+  elseif weather.wind.deg > 56.25  and weather.wind.deg <= 78.75  then wind = wind .. ' ENE'
+  elseif weather.wind.deg > 33.75  and weather.wind.deg <= 56.25  then wind = wind .. ' NE'
+  elseif weather.wind.deg > 11.25  and weather.wind.deg <= 33.75  then wind = wind .. ' NNE'
+  elseif weather.wind.deg > 348.75 and weather.wind.deg <= 11.25  then wind = wind .. ' N'
+  end
+
+  return temp .. '\n' .. conditions .. '\n' .. wind
 end
 
 function run(msg, matches)
   if string.len(matches[1]) > 2 then 
     city = matches[1]
   else
-    city = "Madrid,ES"
+    city = "Kyiv"
   end
   return get_weather(city)
 end
 
 return {
-  description = "weather in that city (Madrid is default)", 
+  description = "weather in that city (Kyiv is default)", 
   usage = "!weather (city)",
-  patterns = {"^!weather(.*)$"}, 
+  patterns = {"^!weather%s?(.*)$"}, 
   run = run 
 }
 
