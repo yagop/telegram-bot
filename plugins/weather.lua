e12fbff193ef1b0b212adbb8a97bced440107869
AckiyBolt <ackiybolt@gmail.com>
2015-04-18 05:18:12 +0300

    weather plg upd

--- plugins/weather.lua
+++ plugins/weather.lua
@@ -9,9 +9,9 @@ function get_weather(location)
   print("Weather returns", weather)
   local city = weather.name
   local country = weather.sys.country
-  local temp = 'The temperature in ' .. city .. ' (' .. country .. ')'
-  temp = temp .. ' is ' .. weather.main.temp .. '°C'
-  conditions = 'Current conditions are: ' .. weather.weather[1].description
+  local temp =  city .. ' (' .. country .. ') '
+  temp = temp .. 'Temp: ' .. weather.main.temp .. '°C'
+  local conditions = 'Conditions: ' .. weather.weather[1].description
   
   if weather.weather[1].main == 'Clear' then
     conditions = conditions .. ' ☀'
@@ -23,25 +23,27 @@ function get_weather(location)
     conditions = conditions .. ' ☔☔☔☔'
   end
 
-  wind = "Wind are " .. weather.wind.speed .. "mps"
+  local wind = "Wind: " 
 
 
-  if     weather.wind.deg > 326.25 and weather.wind.deg <= 348.75 then wind = wind .. ' NNW'
-  elseif weather.wind.deg > 303.75 and weather.wind.deg <= 326.25 then wind = wind .. ' NW'
-  elseif weather.wind.deg > 281.25 and weather.wind.deg <= 303.75 then wind = wind .. ' WNW'
-  elseif weather.wind.deg > 258.75 and weather.wind.deg <= 281.25 then wind = wind .. ' W'
-  elseif weather.wind.deg > 236.25 and weather.wind.deg <= 258.75 then wind = wind .. ' WSW'
-  elseif weather.wind.deg > 213.75 and weather.wind.deg <= 236.25 then wind = wind .. ' SW'
-  elseif weather.wind.deg > 191.25 and weather.wind.deg <= 213.75 then wind = wind .. ' SSW'
-  elseif weather.wind.deg > 168.75 and weather.wind.deg <= 191.25 then wind = wind .. ' S'
-  elseif weather.wind.deg > 146.25 and weather.wind.deg <= 168.75 then wind = wind .. ' SSE'
-  elseif weather.wind.deg > 101.25 and weather.wind.deg <= 123.75 then wind = wind .. ' ESE'
-  elseif weather.wind.deg > 78.75  and weather.wind.deg <= 101.25 then wind = wind .. ' E'
-  elseif weather.wind.deg > 56.25  and weather.wind.deg <= 78.75  then wind = wind .. ' ENE'
-  elseif weather.wind.deg > 33.75  and weather.wind.deg <= 56.25  then wind = wind .. ' NE'
-  elseif weather.wind.deg > 11.25  and weather.wind.deg <= 33.75  then wind = wind .. ' NNE'
-  elseif weather.wind.deg > 348.75 and weather.wind.deg <= 11.25  then wind = wind .. ' N'
+  if     weather.wind.deg > 326.25 and weather.wind.deg <= 348.75 then wind = wind .. 'NNW'
+  elseif weather.wind.deg > 303.75 and weather.wind.deg <= 326.25 then wind = wind .. 'NW'
+  elseif weather.wind.deg > 281.25 and weather.wind.deg <= 303.75 then wind = wind .. 'WNW'
+  elseif weather.wind.deg > 258.75 and weather.wind.deg <= 281.25 then wind = wind .. 'W'
+  elseif weather.wind.deg > 236.25 and weather.wind.deg <= 258.75 then wind = wind .. 'WSW'
+  elseif weather.wind.deg > 213.75 and weather.wind.deg <= 236.25 then wind = wind .. 'SW'
+  elseif weather.wind.deg > 191.25 and weather.wind.deg <= 213.75 then wind = wind .. 'SSW'
+  elseif weather.wind.deg > 168.75 and weather.wind.deg <= 191.25 then wind = wind .. 'S'
+  elseif weather.wind.deg > 146.25 and weather.wind.deg <= 168.75 then wind = wind .. 'SSE'
+  elseif weather.wind.deg > 101.25 and weather.wind.deg <= 123.75 then wind = wind .. 'ESE'
+  elseif weather.wind.deg > 78.75  and weather.wind.deg <= 101.25 then wind = wind .. 'E'
+  elseif weather.wind.deg > 56.25  and weather.wind.deg <= 78.75  then wind = wind .. 'ENE'
+  elseif weather.wind.deg > 33.75  and weather.wind.deg <= 56.25  then wind = wind .. 'NE'
+  elseif weather.wind.deg > 11.25  and weather.wind.deg <= 33.75  then wind = wind .. 'NNE'
+  elseif weather.wind.deg > 348.75 and weather.wind.deg <= 11.25  then wind = wind .. 'N'
   end
+
+  wind = wind .. ' ' .. weather.wind.speed .. "m/s"
 
   return temp .. '\n' .. conditions .. '\n' .. wind
 end
