c27a2e7c92dcbdcaebca0f6bdbbedffd9ed71644
yago <yagoperezs@gmail.com>
2015-04-25 18:34:11 +0200

    vardump uses serpent

--- bot/utils.lua
+++ bot/utils.lua
@@ -116,42 +116,8 @@ function download_to_file(url, file_name)
   return file_path
 end
 
-
-function vardump(value, depth, key)
-  local linePrefix = ""
-  local spaces = ""
-  
-  if key ~= nil then
-    linePrefix = "["..key.."] = "
-  end
-  
-  if depth == nil then
-    depth = 0
-  else
-    depth = depth + 1
-    for i=1, depth do spaces = spaces .. "  " end
-  end
-  
-  if type(value) == 'table' then
-    mTable = getmetatable(value)
-    if mTable == nil then
-      print(spaces ..linePrefix.."(table) ")
-    else
-      print(spaces .."(metatable) ")
-        value = mTable
-    end		
-    for tableKey, tableValue in pairs(value) do
-      vardump(tableValue, depth, tableKey)
-    end
-  elseif type(value)	== 'function' or 
-      type(value)	== 'thread' or 
-      type(value)	== 'userdata' or
-      value		== nil
-  then
-    print(spaces..tostring(value))
-  else
-    print(spaces..linePrefix.."("..type(value)..") "..tostring(value))
-  end
+function vardump(value)
+  print(serpent.block(value, {comment=false}))
 end
 
 -- taken from http://stackoverflow.com/a/11130774/3163199
