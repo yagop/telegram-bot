1b875620e623a556ea1dc53c7f5f8d450bc588dd
yago <yagoperezs@gmail.com>
2015-04-25 18:35:51 +0200

    tabs on scandir

--- bot/utils.lua
+++ bot/utils.lua
@@ -124,8 +124,8 @@ end
 function scandir(directory)
   local i, t, popen = 0, {}, io.popen
   for filename in popen('ls -a "'..directory..'"'):lines() do
-      i = i + 1
-      t[i] = filename
+    i = i + 1
+    t[i] = filename
   end
   return t
 end
