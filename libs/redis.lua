000eda3636839aab015846e7616057293cafd0ea
yago <yagoperezs@gmail.com>
2015-05-01 00:34:52 +0200

    Overwrite HGETALL

--- libs/redis.lua
+++ libs/redis.lua
@@ -5,6 +5,15 @@ local params = {
   port = 6379,
 }
 
+-- Overwrite HGETALL
+Redis.commands.hgetall = Redis.command('hgetall', {
+  response = function(reply, command, ...)
+    local new_reply = { }
+    for i = 1, #reply, 2 do new_reply[reply[i]] = reply[i + 1] end
+    return new_reply
+  end
+})
+
 local redis = nil
 
 -- Won't launch an error if fails
