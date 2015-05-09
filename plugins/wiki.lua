3739ec82efd7b25fe67c88e0ce4c77c2d6cbbb61
DevPGSV <DevPGSV@gmail.com>
2015-05-09 21:03:11 +0200

    Improved wiki plugin

--- plugins/wiki.lua
+++ plugins/wiki.lua
@@ -1,4 +1,8 @@
-local wiki_plugin_usage = "!wiki [terms]: Searches wiki and send results"
+local wiki_plugin_usage = {
+   "!wiki [terms]: Searches wiki and send results",
+   "!wiki_set [wiki]: sets the wikimedia site for this chat",
+   "!wiki_get: gets the current wikimedia site"
+}
 local _file_wiki = './data/wikiSites.lua'
 local _wikiSites = load_from_file(_file_wiki)
 local _defaultWikiSite = "wikipedia.org/wiki"
@@ -38,14 +42,18 @@ local function wikiSearch(searchTerm, chat)
   }
   local body = table.concat(respbody)
   if (status == nil) then return "ERROR: status = nil" end
-  if code ~=200 then return "ERROR: "..code..". Status: "..status end
-  --local wikiContent = string.match(body, "(<div id=\"mw-content-text\"(.*)</p>)")
-  local wikiContent = string.match(body, "<p>(.-)</p>")
-  if (status == nil) then return "ERROR: wikiContent = nil" end
+  if code == 404 then return api.." does not have info about \""..searchTerm.."\"" end 
+  if code ~=200 then return "ERROR: "..status end -- "ERROR: "..code..". Status: "..status
+  if body == nil then return "ERROR: body = nil" end
+  --return "->"..body
+  --local wikiContent = string.match(body, "<div id%=\"mw%-content%-text\"(.*)<div id=\"printfooter\"")
+  local wikiContent = string.match(body, "<div id%=\"mw%-content%-text\".->(.-)<div class=\"printfooter\".->")
+  --local wikiContent = string.match(body, "<p>(.-)</p>")
+  if (wikiContent == nil) then return "ERROR: couldn't parse output." end -- return "ERROR: wikiContent = nil"
+  wikiContent = string.gsub(wikiContent, "\n", "")
   wikiContent = string.gsub(wikiContent, "<a.->", "")
   wikiContent = string.gsub(wikiContent, "</a>", "")
-  wikiContent = string.gsub(wikiContent, "<p.->", "")
-  wikiContent = string.gsub(wikiContent, "</p>", "")
+  wikiContent = string.gsub(wikiContent, "<p.->(.-)</p>", "%1\n")
   wikiContent = string.gsub(wikiContent, "<b.->", "")
   wikiContent = string.gsub(wikiContent, "</b>", "")
   wikiContent = string.gsub(wikiContent, "<i.->", "")
@@ -54,45 +62,59 @@ local function wikiSearch(searchTerm, chat)
   wikiContent = string.gsub(wikiContent, "</sup>", "")
   wikiContent = string.gsub(wikiContent, "<span.->", "")
   wikiContent = string.gsub(wikiContent, "</span>", "")
+  wikiContent = string.gsub(wikiContent, "<div.->(.-)</div>", "%1\n")
   wikiContent = string.gsub(wikiContent, "[.-]", "")
+  wikiContent = string.gsub(wikiContent, "<table.->.-</table>", "")
+  wikiContent = string.gsub(wikiContent, "<li.->(.-)</li>", " - %1\n")
+  wikiContent = string.gsub(wikiContent, "<.->", "")
+
+  --wikiContent = string.gsub(wikiContent, "<.->.-</.->", "")
+  if string.len(wikiContent) >= 500 then
+     wikiContent = string.sub(wikiContent, 1, 500).." [...]"
+  end
+  wikiContent = wikiContent.."\nLink: "..api..parameters
+  --return string.len(wikiContent)
   return wikiContent
 end
 
-local function parseText(chat, text)
-  if (text == nil or text == "!wiki" or text == "!wiki get") then
+local function parseText(chat, msg)
+  local text = msg.text
+  if (text == nil or text == "!wiki" or text == "!wiki_get") then
     return fetch_value(chat)
   end
   
-  action, value = string.match(text, "!wiki (%a+) (.+)")
-  --return text
-  if (action == "set") then
-    if (value == nil) then
-      return wiki_plugin_usage..": nil value"
-    end
-    return save_value(chat, value)
+  local value = string.match(text, "!wiki_set (.*)")
+  if (value ~= nil) then
+     if is_sudo(msg) then
+	return save_value(chat, value)
+     else
+	return "You do not have permission!"
+     end
   end
   
   -- Search here:
   searchTerm = string.match(text, "!wiki (.*)")
-	if (searchTerm == nil) then
-		return wiki_plugin_usage
-	end
+  if (searchTerm == nil) then
+     return wiki_plugin_usage
+  end
   return wikiSearch(searchTerm, chat)
 end
 
 
 local function run(msg, matches)
   local chat_id = tostring(msg.to.id)
-	local result = parseText(chat_id, msg.text)
-	return result
+  local result = parseText(chat_id, msg)
+  return result
 end
 
 return {
   description = "Searches Wikipedia and send results",
   usage = wiki_plugin_usage,
   patterns = {
-    "^!wiki (.*)$",
-    "^!wiki$"
+    "^!wiki .*$",
+    "^!wiki$",
+    "^!wiki_set .*$",
+    "^!wiki_get"
   },
   run = run
 }
