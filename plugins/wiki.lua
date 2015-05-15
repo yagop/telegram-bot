090d4d192737b7be682e54ced05024604514d256
yago <yagoperezs@gmail.com>
2015-05-15 20:03:49 +0200

    Wiki plugin rewritten from scratch

--- plugins/wiki.lua
+++ plugins/wiki.lua
@@ -1,124 +1,123 @@
-local sudo_required_for_wiki_set = false
-local wiki_plugin_usage = {
-   "!wiki [terms]: Searches wiki and send results",
-   "!wiki_set [wiki]: sets the wikimedia site for this chat",
-   "!wiki_get: gets the current wikimedia site"
+-- http://git.io/vUA4M
+
+local JSON = require "cjson"
+
+local Wikipedia = {
+   -- http://meta.wikimedia.org/wiki/List_of_Wikipedias
+   wiki_server = "http://%s.wikipedia.org",
+   wiki_path = "/w/api.php",
+   wiki_params = {
+       action = "query",
+       prop = "extracts",
+       format = "json",
+       exchars = 300,
+       exsectionformat = "plain",
+       explaintext = "",
+       redirects = ""
+   },
+   default_lang = "en",
 }
-local _file_wiki = './data/wikiSites.lua'
-local _wikiSites = load_from_file(_file_wiki)
-local _defaultWikiSite = "wikipedia.org/wiki"
 
-local function save_value(chat, searchSite)
-	if _wikiSites[chat] == nil then _wikiSites[chat] = {} end
-	_wikiSites[chat] = searchSite
-	serialize_to_file(_wikiSites, _file_wiki)
-  return "set "..searchSite.." as search site"
+function Wikipedia:getWikiServer(lang)
+    return string.format(self.wiki_server, lang or self.default_lang)
 end
 
-local function fetch_value(chat)
-  _wikiSites = load_from_file(_file_wiki)
-  if _wikiSites[chat] == nil then
-    save_value(chat, _defaultWikiSite)
-    _wikiSites = load_from_file(_file_wiki)
-    return _defaultWikiSite
-  end
-  return _wikiSites[chat]
+--[[
+--  return decoded JSON table from Wikipedia
+--]]
+function Wikipedia:loadPage(text, lang, intro, plain)
+    local socket = require('socket')
+    local url = require('socket.url')
+    local http = require('socket.http')
+    local https = require('ssl.https')
+    local ltn12 = require('ltn12')
+
+    local request, sink = {}, {}
+    local query = ""
+
+    self.wiki_params.explaintext = plain and "" or nil
+    for k,v in pairs(self.wiki_params) do
+        query = query .. k .. '=' .. v .. '&'
+    end
+    local parsed = url.parse(self:getWikiServer(lang))
+    parsed.path = self.wiki_path
+    parsed.query = query .. "titles=" .. url.escape(text)
+
+    -- HTTP request
+    request['url'] = url.build(parsed)
+    print(request['url'])
+    request['method'] = 'GET'
+    request['sink'] = ltn12.sink.table(sink)
+    http.TIMEOUT, https.TIMEOUT = 10, 10
+    local httpRequest = parsed.scheme == 'http' and http.request or https.request
+    local code, headers, status = socket.skip(1, httpRequest(request))
+
+    if not headers or not sink then
+        return nil
+    end
+
+    local content = table.concat(sink)
+    if content ~= "" then
+        local ok, result = pcall(JSON.decode, content)
+        if ok and result then
+            return result
+        else
+            return nil
+        end
+    else 
+        return nil
+    end
 end
 
+-- extract intro passage in wiki page
+function Wikipedia:wikintro(text, lang)
+    local result = self:loadPage(text, lang, true, true)
 
+    vardump(result)
 
-local function wikiSearch(searchTerm, chat)
-  local api = "http://"..fetch_value(chat)
-  if string.sub(api, -1) ~= "/" then api = api.."/" end
-  local parameters = (URL.escape(searchTerm) or "")
-  local url = api..parameters
-  local http = require("socket.http")
-  local respbody = {} 
-  local ltn12 = require "ltn12"
-  local body, code, headers, status = http.request{
-    url = url,
-    method = "GET",
-    --headers = headers,
-    --source = ltn12.source.string(reqBody),
-    redirect = true,
-    sink = ltn12.sink.table(respbody)
-  }
-  --for key, value in pairs (headers) do
-  --  print(key.."->"..value)
-  --end
-  
-  
-  local body = table.concat(respbody)
-  if (status == nil) then return "ERROR: status = nil" end
-  if code == 404 then return api.." does not have info about \""..searchTerm.."\"" end 
-  if code ~=200 then return "ERROR: "..status end -- "ERROR: "..code..". Status: "..status
-  if body == nil then return "ERROR: body = nil" end
-  local location = headers.location
-  local wikiContent = string.match(body, "<div id%=\"mw%-content%-text\".->(.-)<div class=\"printfooter\".->")
-  if (wikiContent == nil) then return "ERROR: couldn't parse output." end -- return "ERROR: wikiContent = nil"
-  wikiContent = string.gsub(wikiContent, "\n", "")
-  wikiContent = string.gsub(wikiContent, "<a.->", "")
-  wikiContent = string.gsub(wikiContent, "</a>", "")
-  wikiContent = string.gsub(wikiContent, "<p.->(.-)</p>", "%1\n")
-  wikiContent = string.gsub(wikiContent, "<b.->", "")
-  wikiContent = string.gsub(wikiContent, "</b>", "")
-  wikiContent = string.gsub(wikiContent, "<i.->", "")
-  wikiContent = string.gsub(wikiContent, "</i>", "")
-  wikiContent = string.gsub(wikiContent, "<sup.->", "")
-  wikiContent = string.gsub(wikiContent, "</sup>", "")
-  wikiContent = string.gsub(wikiContent, "<span.->", "")
-  wikiContent = string.gsub(wikiContent, "</span>", "")
-  wikiContent = string.gsub(wikiContent, "<div.->(.-)</div>", "%1\n")
-  wikiContent = string.gsub(wikiContent, "[.-]", "")
-  wikiContent = string.gsub(wikiContent, "<table.->.-</table>", "")
-  wikiContent = string.gsub(wikiContent, "<li.->(.-)</li>", " - %1\n")
-  wikiContent = string.gsub(wikiContent, "<.->", "")
-  --wikiContent = string.gsub(wikiContent, "<.->.-</.->", "")
-  if string.len(wikiContent) >= 500 then
-     wikiContent = string.sub(wikiContent, 1, 500).." [...]"
-  end
-  wikiContent = wikiContent.."\nLink: "..location
-  return wikiContent
-end
+    if result and result.query then
 
-local function parseText(chat, msg)
-  local text = msg.text
-  if (text == nil or text == "!wiki" or text == "!wiki_get") then
-    return fetch_value(chat)
-  end
-  
-  local value = string.match(text, "!wiki_set (.*)")
-  if (value ~= nil) then
-     if (is_sudo(msg) or (not sudo_required_for_wiki_set)) then
-        return save_value(chat, value)
-     else
-        return "You do not have permission!"
-     end
-  end
-  
-  -- Search here:
-  searchTerm = string.match(text, "!wiki (.*)")
-  if (searchTerm == nil) then
-     return wiki_plugin_usage
-  end
-  return wikiSearch(searchTerm, chat)
+        local query = result.query
+        if query and query.normalized then
+            text = query.normalized[1].to or text
+        end
+
+        local page = query.pages[next(query.pages)]
+
+        if page and page.extract then
+            return text..": "..page.extract
+        else
+            return "Extract not found for "..text
+        end
+    else
+        return "Sorry an error happened"
+    end
 end
 
 
 local function run(msg, matches)
-  local chat_id = tostring(msg.to.id)
-  local result = parseText(chat_id, msg)
-  return result
+    -- TODO: Remember language (i18 on future version)
+    -- TODO: Support for non Wikipedias but Mediawikis
+    local term = matches[2]
+    local lang = matches[1] -- Can be nil
+    if not term then
+        term = lang
+        lang = nil
+    end
+    -- TODO: Show the link
+    local result = Wikipedia:wikintro(term, lang)
+    return result
 end
 
 return {
-  description = "Searches Wikipedia and send results",
-  usage = wiki_plugin_usage,
-  patterns = {
-    "^!wiki .*$",
-    "^!wiki$",
-    "^!wiki_set .*$",
-    "^!wiki_get"
-  },
-  run = run
+    description = "Searches Wikipedia and send results",
+    usage = {
+        "!wiki [text]: Search on default Wikipedia (EN)",
+        "!wiki(lang) [text]: Search on 'lang' wikipedia. Example: !wikies hola.",
+    },
+    patterns = {
+        "^!wiki (.*)$",
+        "^!wiki(%w+) (.*)$",
+    },
+    run = run
 }
