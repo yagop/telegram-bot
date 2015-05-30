121c816b917ac8953d5f5717dc02f606f4080053
yago <yagoperezs@gmail.com>
2015-05-30 14:38:59 +0200

    Ask for users ids on other chat

--- plugins/id.lua
+++ plugins/id.lua
@@ -13,10 +13,14 @@ local function user_print_name(user)
 end
 
 local function returnids(cb_extra, success, result)
-   local receiver = "chat#id" .. result.id
+   local receiver = cb_extra.receiver
+   local chat_id = "chat#id"..result.id
    local chatname = result.print_name
-   local text = 'IDs for chat ' .. chatname .. ' (' .. receiver .. ')\n'
-   text = text .. "There are " .. result.members_num .. ' members\n---------\n'
+
+   local text = 'IDs for chat '..chatname
+      ..' ('..chat_id..')\n'
+      ..'There are '..result.members_num..' members'
+      ..'\n---------\n'
    for k,v in pairs(result.members) do
       text = text .. v.print_name .. " (user#id" .. v.id .. ")\n"
    end
@@ -24,6 +28,7 @@ local function returnids(cb_extra, success, result)
 end
 
 local function run(msg, matches)
+   local receiver = get_receiver(msg)
    if matches[1] == "!id" then
       local text = user_print_name(msg.from) .. ' (user#id' .. msg.from.id .. ')'
       if is_chat_msg(msg) then
@@ -31,11 +36,17 @@ local function run(msg, matches)
       end
       return text
    elseif matches[1] == "chat" then
-      if not is_chat_msg(msg) then
-         return "You are not in a group."
+      -- !ids? (chat) (%d+)
+      if matches[2] and is_sudo(msg) then
+         local chat = 'chat#id'..matches[2]
+         chat_info(chat, returnids, {receiver=receiver})
+      else
+         if not is_chat_msg(msg) then
+            return "You are not in a group."
+         end
+         local chat = get_receiver(msg)
+         chat_info(chat, returnids, {receiver=receiver})
       end
-      local chat_name = msg.to.print_name
-      chat_info(chat_name, returnids, {})
    end
 end
 
@@ -47,7 +58,8 @@ return {
    },
    patterns = {
       "^!id$",
-      "^!ids? (chat)"
+      "^!ids? (chat) (%d+)$",
+      "^!ids? (chat)$"
    },
    run = run
 }
