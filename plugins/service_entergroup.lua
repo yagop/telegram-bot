2b73faa77ab78d62b3836b17848a4af886308047
Fauzi NS <silenceangel41@gmail.com>
2015-08-14 20:14:09 +0700

    left - good bye!

--- plugins/service_entergroup.lua
+++ plugins/service_entergroup.lua
@@ -87,6 +87,9 @@ local function run(msg, matches)
       end
       chat_new_user_link(msg)
       description_rules(msg, nama)
+   elseif matches[1] == "chat_del_user" then
+       local bye_name = msg.action.user.first_name
+       return 'Bye '..bye_name..'!'
    end
 end
 
@@ -96,6 +99,7 @@ return {
    patterns = {
       "^!!tgservice (chat_add_user)$",
       "^!!tgservice (chat_add_user_link)$",
+      "^!!tgservice (chat_del_user)$",
    },
    run = run
 }
