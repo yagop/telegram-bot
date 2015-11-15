68d19c7377a69b9dbbe61f290a09bf9beb900b42
Sahri Riza Umami <riza@openmailbox.org>
2015-11-15 12:44:22 +0700

    Anti flood have options to kick or ban flooders.

--- plugins/anti-flood.lua
+++ plugins/anti-flood.lua
@@ -1,83 +1,106 @@
-local NUM_MSG_MAX = 5 -- Max number of messages per TIME_CHECK seconds
+-- data saved to moderation.json
+do
+
+-- make sure to set with value that not higher than stats.lua
+local NUM_MSG_MAX = 5  -- Max number of messages per TIME_CHECK seconds
 local TIME_CHECK = 5
 
-local function kick_user(user_id, chat_id)
-  local chat = 'chat#id'..chat_id
-  local user = 'user#id'..user_id
-  chat_del_user(chat, user, function (data, success, result)
-    if success ~= 1 then
-      local text = 'I can\'t kick '..data.user..' but should be kicked'
-      send_msg(data.chat, '', ok_cb, nil)
-    end
-  end, {chat=chat, user=user})
+local function is_anti_flood(msg)
+	local data = load_data(_config.moderation.data)
+	local anti_flood_stat = data[tostring(msg.to.id)]['settings']['anti_flood']
+	if anti_flood_stat == 'kick' or anti_flood_stat == 'ban' then
+		return true
+	else
+		return false
+	end
 end
 
-local function run (msg, matches)
-  if msg.to.type ~= 'chat' then
-    return 'Anti-flood works only on channels'
-  else
-    local chat = msg.to.id
-    local hash = 'anti-flood:enabled:'..chat
-    if matches[1] == 'enable' then
-      redis:set(hash, true)
-      return 'Anti-flood enabled on chat'
-    end
-    if matches[1] == 'disable' then
-      redis:del(hash)
-      return 'Anti-flood disabled on chat'
-    end
-  end
-end
+local function pre_process(msg)
 
-local function pre_process (msg)
-  -- Ignore service msg
-  if msg.service then
-    print('Service message')
-    return msg
-  end
-
-  local hash_enable = 'anti-flood:enabled:'..msg.to.id
-  local enabled = redis:get(hash_enable)
-
-  if enabled then
-    print('anti-flood enabled')
-    -- Check flood
-    if msg.from.type == 'user' then
-      -- Increase the number of messages from the user on the chat
-      local hash = 'anti-flood:'..msg.from.id..':'..msg.to.id..':msg-num'
-      local msgs = tonumber(redis:get(hash) or 0)
-      if msgs > NUM_MSG_MAX then
-        local receiver = get_receiver(msg)
-        local user = msg.from.id
-        local text = 'User '..user..' is flooding'
-        local chat = msg.to.id
-
-        send_msg(receiver, text, ok_cb, nil)
-        if msg.to.type ~= 'chat' then
-          print("Flood in not a chat group!")
-        elseif user == tostring(our_id) then
-          print('I won\'t kick myself')
-        elseif is_sudo(msg) then
-          print('I won\'t kick an admin!')
-        else
-          kick_user(user, chat)
-        end
+  local user = msg.from.id
+  local chat = msg.to.id
+  local hash = 'floodc:'..user..':'..chat
+  redis:incr(hash)
+  if msg.from.type == 'user' then
+    local hash = 'user:'..user..':floodc'
+    local msgs = tonumber(redis:get(hash) or 0)
+    local text = 'User '..user..' is flooding'
+    if msgs > NUM_MSG_MAX and not is_sudo(msg) then
+      local data = load_data(_config.moderation.data)
+      local anti_flood_stat = data[tostring(chat)]['settings']['anti_flood']
+      if anti_flood_stat == 'kick' then
+        send_large_msg(get_receiver(msg), text)
+        chat_del_user('chat#id'..chat, 'user#id'..user, ok_cb, true)
+        msg = nil
+      elseif anti_flood_stat == 'ban' then
+        send_large_msg(get_receiver(msg), text)
+        -- Save to redis
+        local hash =  'banned:'..chat..':'..user
+        redis:set(hash, true)
+        chat_del_user('chat#id'..chat, 'user#id'..user, ok_cb, true)
+        send_msg('chat#id'..chat, 'User '..user..' banned', ok_cb,  true)
         msg = nil
       end
-      redis:setex(hash, TIME_CHECK, msgs+1)
     end
+    redis:setex(hash, TIME_CHECK, msgs+1)
   end
   return msg
 end
 
+function run(msg, matches)
+
+  if not is_sudo(msg) then
+    return "For moderators only!"
+  end
+
+  if matches[1] == 'antiflood' then
+  local data = load_data(_config.moderation.data)
+    if matches[2] == 'kick' then
+      local anti_flood_stat = data[tostring(msg.to.id)]['settings']['anti_flood']
+      if anti_flood_stat == 'kick' then
+        return 'Anti flood protection already enabled.\nFlooder will be kicked.'
+      else
+        data[tostring(msg.to.id)]['settings']['anti_flood'] = 'kick'
+        save_data(_config.moderation.data, data)
+      end
+      return 'Anti flood  protection has been enabled.\nFlooder will be kicked.'
+    end
+    if matches[2] == 'ban' then
+      local anti_flood_stat = data[tostring(msg.to.id)]['settings']['anti_flood']
+      if anti_flood_stat == 'ban' then
+        return 'Anti flood  protection already enabled.\nFlooder will be banned.'
+      else
+        data[tostring(msg.to.id)]['settings']['anti_flood'] = 'ban'
+        save_data(_config.moderation.data, data)
+      end
+      return 'Anti flood  protection has been enabled.\nFlooder will be banned.'
+    end
+    if matches[2] == 'disable' then
+      local anti_flood_stat = data[tostring(msg.to.id)]['settings']['anti_flood']
+      if anti_flood_stat == 'no' then
+        return 'Anti flood  protection is not enabled.'
+      else
+        data[tostring(msg.to.id)]['settings']['anti_flood'] = 'no'
+        save_data(_config.moderation.data, data)
+        return 'Anti flood  protection has been disabled.'
+      end
+    end
+  end
+end
+
 return {
-  description = 'Plugin to kick flooders from group.',
-  usage = {},
+  description = "Plugin to kick flooders from group.",
+  usage = {
+    "!antiflood <kick> : Enable flood protection. Flooder will be kicked.",
+    "!antiflood <ban> : Enable flood protection. Flooder will be banned.",
+    "!antiflood <disable> : Disable flood protection"
+  },
   patterns = {
-    '^!antiflood (enable)$',
-    '^!antiflood (disable)$'
+    "^!(antiflood) (.*)$"
   },
   run = run,
   privileged = true,
   pre_process = pre_process
 }
+
+end
