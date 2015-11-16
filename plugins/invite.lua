d292f06b6c2456f55f7ce0d3373b0fc3858a674e
Sahri Riza Umami <riza@openmailbox.org>
2015-11-16 12:13:24 +0700

    Invite user by @username.

--- plugins/invite.lua
+++ plugins/invite.lua
@@ -1,46 +1,57 @@
 -- Invite other user to the chat group.
--- Use !invite name User_name or !invite id id_number
--- The User_name is the print_name (there are no spaces but _)
+-- Use !invite @username to invite a user by @username
+-- Use !invite ********* (where ********* is id_number) to invite a user by id_number
+-- Use !invite Type print_name Here to invite a user by print_name
 
 do
 
+-- Think it's kind of useless. Just to suppress '*** lua: attempt to call a nil value'
 local function callback(extra, success, result)
   vardump(success)
   vardump(result)
 end
 
+local function res_user_callback(extra, success, result)
+  local user = 'user#id'..result.id
+  local chat = extra.chat
+  local text = extra.text
+  if success == 0 then
+    return send_large_msg(chat, "Can't invite user to this group.")
+  end
+  chat_add_user(chat, user, callback, false)
+  return text
+end
+
 local function run(msg, matches)
-  local user = matches[2]
-
-  -- User submitted a user name
-  if matches[1] == "name" then
-    user = string.gsub(user," ","_")
-  end
-  
-  -- User submitted an id
-  if matches[1] == "id" then
-    user = 'user#id'..user
-  end
-
-  -- The message must come from a chat group
+  local user = matches[1]
+  local chat = 'chat#id'..msg.to.id
+  local text = "Add: "..user.." to "..chat
   if msg.to.type == 'chat' then
-    local chat = 'chat#id'..msg.to.id
-    chat_add_user(chat, user, callback, false)
-    return "Add: "..user.." to "..chat
+    if string.match(user, '^%d+$') then
+      user = 'user#id'..user
+      chat_add_user(chat, user, callback, false)
+      return text
+    elseif string.match(user, '^@.+$') then
+      username = string.gsub(user, '@', '')
+      msgr = res_user(username, res_user_callback, {chat=chat, text=text})
+    else
+      user = string.gsub(user, ' ', '_')
+      chat_add_user(chat, user, callback, false)
+      return text
+    end
   else 
     return 'This isnt a chat group!'
   end
-
 end
 
 return {
-  description = "Invite other user to the chat group", 
+  description = "Invite other user to the chat group.",
   usage = {
-    '!invite name [user_name]', 
-    '!invite id [user_id]'},
+    "!invite [id|user_name|name]",
+  },
   patterns = {
-    "^!invite (name) (.*)$",
-    "^!invite (id) (%d+)$"
+    "^!invite (.*)$",
+    "^!invite (%d+)$"
   }, 
   run = run 
 }
