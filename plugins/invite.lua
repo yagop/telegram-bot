53c49dd38f5713625d9fc8e57a4f286746657863
Sahri Riza Umami <riza@openmailbox.org>
2015-11-24 13:41:41 +0700

    Fix _attempt to index local_ error in invite.lua.

--- plugins/invite.lua
+++ plugins/invite.lua
@@ -1,51 +1,60 @@
--- Invite other user to the chat group.
--- Use !invite @username to invite a user by @username
--- Use !invite ********* (where ********* is id_number) to invite a user by id_number
--- Use !invite Type print_name Here to invite a user by print_name
+--[[
+Invite other user to the chat group.
+
+Use !invite ********* (where ********* is id_number) to invite a user by id_number.
+This is the most reliable method.
+
+Use !invite @username to invite a user by @username.
+Less reliable. Some users don't have @username.
+
+Use !invite Type print_name Here to invite a user by print_name.
+Unreliable. Avoid if possible.
+]]--
 
 do
 
 -- Think it's kind of useless. Just to suppress '*** lua: attempt to call a nil value'
 local function callback(extra, success, result)
-  if success == 0 then
-    return send_large_msg(chat, "Can't invite user to this group.")
-  else
+  if success == 1 and extra ~= false then
     return extra.text
+  else
+    return send_large_msg(chat, "Can't invite user to this group.")
   end
 end
 
-local function res_user_callback(extra, success, result)
-  local user = 'user#id'..result.id
+local function resolve_username(extra, success, result)
   local chat = extra.chat
-  if success == 0 then
+  if success == 1 then
+    local user = 'user#id'..result.id
+    chat_add_user(chat, user, callback, false)
+    return extra.text
+  else
     return send_large_msg(chat, "Can't invite user to this group.")
   end
-  chat_add_user(chat, user, callback, false)
-  return extra.text
 end
 
 local function run(msg, matches)
-  local user = matches[1]
+  local user_id = matches[1]
   local chat = 'chat#id'..msg.to.id
-  local text = "Add: "..user.." to "..chat
+  local text = "Add: "..user_id.." to "..chat
   if msg.to.type == 'chat' then
-    if string.match(user, '^%d+$') then
-      user = 'user#id'..user
+    if string.match(user_id, '^%d+$') then
+      user = 'user#id'..user_id
       chat_add_user(chat, user, callback, {chat=chat, text=text})
-    elseif string.match(user, '^@.+$') then
-      username = string.gsub(user, '@', '')
-      msgr = res_user(username, res_user_callback, {chat=chat, text=text})
+    elseif string.match(user_id, '^@.+$') then
+      username = string.gsub(user_id, '@', '')
+      msgr = res_user(username, resolve_username, {chat=chat, text=text})
     else
-      user = string.gsub(user, ' ', '_')
+      user = string.gsub(user_id, ' ', '_')
       chat_add_user(chat, user, callback, {chat=chat, text=text})
     end
-  else 
+  else
     return 'This isnt a chat group!'
   end
 end
 
 return {
-  description = "Invite other user to the chat group.",
+  description = 'Invite other user to the chat group.',
   usage = {
     -- need space in front of this, so bot won't consider it as a command
     ' !invite [id|user_name|name]'
@@ -53,7 +62,7 @@ return {
   patterns = {
     "^!invite (.*)$",
     "^!invite (%d+)$"
-  }, 
+  },
   run = run,
   privileged = true
 }
