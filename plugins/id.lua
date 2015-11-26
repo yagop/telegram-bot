7d053e2664cd45957b6ae896ff4b8fc26b03fbba
Sahri Riza Umami <riza@openmailbox.org>
2015-11-26 13:44:45 +0700

    Change returnids print_name to first_name + last_name again.

--- plugins/id.lua
+++ plugins/id.lua
@@ -19,7 +19,7 @@ local function scan_name(extra, success, result)
   for k,member in pairs(result.members) do
     local fields = {'first_name', 'print_name', 'username'}
       for k,field in pairs(fields) do
-        if member[field] and type(member[field]) == "string" then
+        if member[field] and type(member[field]) == 'string' then
           if member[field]:match(extra.user) then
             local id = tostring(member.id)
             founds[id] = member
@@ -28,18 +28,18 @@ local function scan_name(extra, success, result)
       end
     end
     if next(founds) == nil then -- Empty table
-      send_msg(extra.receiver, extra.user.." not found on this chat.", ok_cb, false)
+      send_msg(extra.receiver, extra.user..' not found on this chat.', ok_cb, false)
     else
-      local text = ""
+      local text = ''
       for k,user in pairs(founds) do
-        local first_name = user.first_name or ""
-        local print_name = user.print_name or ""
-        local user_name = user.user_name or ""
-        local id = user.id  or "" -- This would be funny
-        text = text.."First name: "..first_name.."\n"
-            .."Print name: "..print_name.."\n"
-            .."User name: "..user_name.."\n"
-            .."ID: "..id.."\n\n"
+        local first_name = user.first_name or ''
+        local print_name = user.print_name or ''
+        local user_name = user.user_name or ''
+        local id = user.id  or '' -- This would be funny
+        text = text..'First name: '..first_name..'\n'
+            ..'Print name: '..print_name..'\n'
+            ..'User name: '..user_name..'\n'
+            ..'ID: '..id..'\n\n'
       end
     send_msg(extra.receiver, text, ok_cb, false)
   end
@@ -47,9 +47,9 @@ end
 
 local function res_user_callback(extra, success, result)
   if success == 1 then
-    send_msg(extra.receiver, "ID for "..extra.user.." is: "..result.id, ok_cb, false)
+    send_msg(extra.receiver, 'ID for '..extra.user..' is: '..result.id, ok_cb, false)
   else
-    send_msg(extra.receiver, extra.user.." not found on this chat.", ok_cb, false)
+    send_msg(extra.receiver, extra.user..' not found on this chat.', ok_cb, false)
   end
 end
 
@@ -66,11 +66,17 @@ local function returnids(extra, success, result)
   i = 0
   for k,v in pairs(result.members) do
     i = i+1
-    text = text..i..'. '..v.id..' ⋆ '..string.gsub(v.print_name, '_', ' ')..'\n'
-
--- Too large. Need a way to split the text.
---    text = text .. i .. '. '..v.id..' ⋆ @'..(v.username or '')..' ⋆ '
---           ..(v.first_name or '')..' '..(v.last_name or '')..'\n'
+    if v.last_name then
+      last_name = ' '..v.last_name
+    else
+      last_name = ''
+    end
+    if v.username then
+      user_name = ' @'..v.username
+    else
+      user_name = ''
+    end
+    text = text..i..'. ['..v.id..'] '..user_name..' '..v.first_name..last_name..'\n'
   end
   send_large_msg(extra.receiver, text)
 end
@@ -78,7 +84,7 @@ end
 local function run(msg, matches)
   local receiver = get_receiver(msg)
   local user = matches[1]
-  local text = "ID for "..user.." is: "
+  local text = 'ID for '..user..' is: '
   if msg.to.type == 'chat' then
     if msg.text == '!id' then
       if msg.reply_id then
@@ -86,12 +92,12 @@ local function run(msg, matches)
       else
         local text = 'Name : '..(msg.from.first_name or '')..' '..(msg.from.last_name or '')..'\n'
                      ..'ID : ' .. msg.from.id
-        local text = text.."\n\nYou are in group "
-                     ..string.gsub(user_print_name(msg.to), '_', ' ').." (ID: "
-                     .. msg.to.id  .. ")"
+        local text = text..'\n\nYou are in group '
+                     ..string.gsub(user_print_name(msg.to), '_', ' ')..' (ID: '
+                     ..msg.to.id..')'
         return text
       end
-    elseif matches[1] == "chat" then
+    elseif matches[1] == 'chat' then
       if matches[2] and is_sudo(msg) then
         local chat = 'chat#id'..matches[2]
         chat_info(chat, returnids, {receiver=receiver})
@@ -111,15 +117,15 @@ local function run(msg, matches)
 end
 
 return {
-  description = "Know your id or the id of a chat members.",
+  description = 'Know your id or the id of a chat members.',
   usage = {
-    "!id: Return your ID and the chat id if you are in one.",
-    "!id: Return ID of replied user if used by reply.",
-    "!id chat: Return the IDs of the current chat members.",
-    "!id chat <chat_id>: Return the IDs of the current <chat_id> members.",
-    "!id <id>: Return the IDs of the <id>.",
-    "!id @<user_name>: Return the member @<user_name> ID from the current chat.",
-    "!id <text>: Search for users with <text> on print_name on current chat."
+    '!id: Return your ID and the chat id if you are in one.',
+    '!id: Return ID of replied user if used by reply.',
+    '!id chat: Return the IDs of the current chat members.',
+    '!id chat <chat_id>: Return the IDs of the current <chat_id> members.',
+    '!id <id>: Return the IDs of the <id>.',
+    '!id @<user_name>: Return the member @<user_name> ID from the current chat.',
+    '!id <text>: Search for users with <text> on print_name on current chat."
   },
   patterns = {
     "^!id$",
