6642748f4de334395512af9e8608c16726688715
ALIATAEE <A.ATAEE1381@GMAIL.COM>
2016-03-22 20:19:03 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {our_id},
+    sudo_users = {155 098 899},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
