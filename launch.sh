282a0de3e001af1e11915808da85c4b9dd373da2
Yago <yago.ftw@gmail.com>
2016-06-29 21:12:02 +0200

    Disable patch

--- launch.sh
+++ launch.sh
@@ -77,7 +77,7 @@ install_rocks() {
 install() {
   git pull
   git submodule update --init --recursive
-  patch -i "patches/disable-python-and-libjansson.patch" -p 0 --batch --forward
+  # patch -i "patches/disable-python-and-libjansson.patch" -p 0 --batch --forward
   RET=$?;
 
   cd tg
